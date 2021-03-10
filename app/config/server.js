d99c0252614a04a1f1fff32162f3cd255e2ffde1
César Augusto <cesar.augusto@seoze.com>
2021-03-10 15:28:06 -0300

    first blood

---
+++ app/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '39d68fcbe35761131e2ce12c9ba15001'),
+    },
+  },
+});
---
+++ app/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '827c2fa4-a849-4c4e-a3fd-71ff4376cb01'
+};
